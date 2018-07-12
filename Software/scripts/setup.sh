301df45123a4f111144e33f76195013e613dd961
Bryan Nielsen <bnielsen1965@gmail.com>
2018-07-12 07:45:19 -0700

    Removed git install script because it must be part of setup.

--- Software/scripts/setup.sh
+++ Software/scripts/setup.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-NODEJS_DLPATH="https://nodejs.org/dist/latest-v8.x/"
-NODEJS_TAR_EXT=".tar.xz"
-ARCH=$(uname -m)
-NODEJS_DLPATTERN="node-[^>]*-linux-$ARCH$NODEJS_TAR_EXT"
-NODEJS_INSTALL_PATH="/opt/nodejs"
+REPO_NAME="Explorer_HAT_Test_Fixture"
+REPO_URL="https://github.com/bnielsen1965/$REPO_NAME.git"
+REPO_SCRIPTS_PATH="/Software/scripts"
 
 
 # display a green message
@@ -20,7 +18,7 @@ errormessage() {
 
 
 # check if a command is installed
-installed() {
+commandInstalled() {
 	# check if command exists and is executable
 	if [ -x "$(command -v $1)" ]; then
 		return 0
@@ -29,45 +27,54 @@ installed() {
 }
 
 
-# get nodejs tarball and install
-installnodejs() {
-	infomessage "Download nodejs"
-	NODEJS_FILE=$(wget -O - "$NODEJS_DLPATH" | grep -o $NODEJS_DLPATTERN | head -1)
-	if [ $? -ne 0 ]; then
-		errormessage "Failed to find nodejs download file."
-		exit 1
-	fi
-	NODEJS_TMP_DIR=$(mktemp -d)
-	NODEJS_TMP_FILE="$NODEJS_TMP_DIR/$NODEJS_FILE"
-	wget -O $NODEJS_TMP_FILE "$NODEJS_DLPATH$NODEJS_FILE"
-	if [ $? -ne 0 ]; then
-                errormessage "Failed to download nodejs file."
-                exit 1
-        fi
 
-	infomessage "Extract nodejs."
-	cd "$NODEJS_TMP_DIR"
-	sudo mkdir -p "$NODEJS_INSTALL_PATH"
-	sudo tar xf "$NODEJS_FILE" -C "$NODEJS_INSTALL_PATH/"
-	if [ $? -ne 0 ]; then
-                errormessage "Failed to extract tarball."
-                exit 1
-        fi
-
-	infomessage "Create symlinks."
-	NODEJS_DIR="$NODEJS_INSTALL_PATH/${NODEJS_FILE%$NODEJS_TAR_EXT}"
-	sudo ln -s "$NODEJS_DIR/bin/node" /usr/bin/node
-	sudo ln -s "$NODEJS_DIR/bin/npm" /usr/bin/npm
-
-	infomessage "Clean up."
-	rm -rf "$NODEJS_TMP_DIR"
+# install git if not already installed
+installGit() {
+	sudo apt-get update
+	sudo apt-get install git -y
 }
 
 
-if installed node && installed npm; then
-	infomessage "node and npm commands already installed."
+cloneRepo() {
+	infomessage "clone"
+	REPO_TMP_DIR=$(mktemp -d)
+	if [ $? -ne 0 ]; then
+		errormessage "Failed to create tmp directory for clone."
+		return 1
+	fi
+
+	cd "$REPO_TMP_DIR"
+	git clone "$REPO_URL"
+	if [ $? -ne 0 ]; then
+		errormessage "Error while cloning repo. $REPO_URL"
+		return 1
+	fi
+
+	REPO_DIR="$REPO_TMP_DIR/$REPO_NAME"
+
+	return 0
+}
+
+
+runSetupScripts() {
+	cd "$REPO_DIR$REPO_SCRIPTS_PATH"
+	# use array and run $v-install.sh  [ 'nodejs', 'overlay', 'pi-buttons']
+}
+
+
+# make sure we have git
+if commandInstalled git; then
+	infomessage "git already installed."
 else
-	infomessage "Install node and npm."
-	installnodejs
+	infomessage "Install git."
+	installGit
 fi
 
+# clone the project repo
+if cloneRepo; then
+	runSetupScripts
+	exit 0
+else
+	errormessage "Setup failed!"
+	exit 1
+fi
