bd66f30e66c5e74752110a9fc77b5dd90ca9f8ae
Bryan Nielsen <bnielsen1965@gmail.com>
2018-07-22 13:59:44 -0700

    Added i2c and spi settings to setup scripts.

--- Software/scripts/overlay-install.sh
+++ Software/scripts/overlay-install.sh
@@ -6,7 +6,8 @@
 BOOT_CONFIG="/boot/config.txt"
 OVERLAY_SOURCE="../overlay/exhattf.dtbo"
 OVERLAY_DEST="/boot/overlays/exhattf.dtbo"
-
+BOOT_I2C_SETTINGS="i2c1=on,i2c1_baudrate=400000"
+BOOT_SPI_SETTINGS="spi=on"
 
 bootvariableset() {
 	FIND="^\s*$1\s*=\s*$2\s*[#]*.*$"
@@ -25,9 +26,33 @@ installoverlay() {
 	echo "dtoverlay=exhattf" | sudo tee -a "$BOOT_CONFIG" > /dev/null
 }
 
+i2cbootsettings() {
+	infomessage "Setting i2c boot settings."
+	echo "" | sudo tee -a "$BOOT_CONFIG" > /dev/null
+	echo "dtparam=$BOOT_I2C_SETTINGS" | sudo tee -a "$BOOT_CONFIG" > /dev/null
+}
+
+spibootsettings() {
+	infomessage "Setting spi boot settings."
+	echo "" | sudo tee -a "$BOOT_CONFIG" > /dev/null
+	echo "dtparam=$BOOT_SPI_SETTINGS" | sudo tee -a "$BOOT_CONFIG" > /dev/null
+}
+
 if bootvariableset "dtoverlay" "exhattf"; then
 	infomessage "Overlay already installed."
 else
 	infomessage "Install overlay"
 	installoverlay
+fi
+
+if bootvariableset "dtparam" "$BOOT_I2C_SETTINGS"; then
+	infomessage "I2C boot settings already set."
+else
+	i2cbootsettings
+fi
+
+if bootvariableset "dtparam" "$BOOT_SPI_SETTINGS"; then
+	infomessage "SPI boot settings already set."
+else
+	spibootsettings
 fi
